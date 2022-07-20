ec55ae61e684cee62e863d39e321ad7feadb6524
Hemant <hemantdhurve007@gmail.com>
2022-07-20 11:05:31 +0530

    added Test Case Using Generics files

---
+++ UC3_MaxString/UC3_MaxString/MaxThreeString.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UC3_MaxString
+{
+    class MaxThreeString
+    {
+        public static string MaxThreeStrings(string firstvalue, string secondvalue, string thirdvalue)
+        {
+            if (firstvalue.CompareTo(secondvalue) > 0 && (firstvalue.CompareTo(thirdvalue) > 0))
+            {
+                return firstvalue;
+            }
+            if (secondvalue.CompareTo(firstvalue) > 0 && (secondvalue.CompareTo(thirdvalue) > 0))
+            {
+                return secondvalue;
+            }
+            if (thirdvalue.CompareTo(firstvalue) > 0 && (thirdvalue.CompareTo(secondvalue) > 0))
+            {
+                return thirdvalue;
+            }
+            throw new Exception("VALUES ARE SAME");
+        }
+    }
+}
---
+++ UC3_MaxString/UC3_MaxString/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace UC3_MaxString
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello and welcome to Generics");
+
+            //// // create string object 
+            ////in T.C 1 
+            //Console.WriteLine("Using generics find String  among three");
+            //string StringMaxNumber = MaxThreeString.MaxThreeStrings("Apple", "Peach", "Banana");
+            //Console.WriteLine("Maximum value among them is :" + StringMaxNumber);
+
+            //in T.C 2 
+            Console.WriteLine("Using generics find String  among three");
+            string StringMaxNumber = MaxThreeString.MaxThreeStrings("Peach", "Apple", "Banana");
+            Console.WriteLine("Maximum value among them is :" + StringMaxNumber);
+
+            ////in T.C 3 
+            //Console.WriteLine("Using generics find String  among three");
+            //string StringMaxNumber = MaxThreeString.MaxThreeStrings("Apple", "Peach", "Banana");
+            //Console.WriteLine("Maximum value among them is :" + StringMaxNumber);
+        }
+    }
+}
+
+
