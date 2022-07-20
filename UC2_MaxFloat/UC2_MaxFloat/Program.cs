22d4be28d51030aaa1bae96c934348f49333ac1c
Hemant <hemantdhurve007@gmail.com>
2022-07-20 11:03:04 +0530

    added Test Case Using Generics files

---
+++ UC2_MaxFloat/UC2_MaxFloat/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace UC2_MaxFloat
+{
+    public class program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello and welcome to Generics");
+            // T.C 1st position number is maximim
+            Console.WriteLine("Using generics find maximum float number among three");
+            float FloatMaxNumber = MaxNumberFind.MaxThreeFloat(141.1f, 29.6f, 34.5f);
+            Console.WriteLine("Maximum value among them is :" + FloatMaxNumber);
+
+            // T.C 2st position number is maximim
+            //Console.WriteLine("Using generics find maximum float number among three");
+            //float FloatMaxNumber = MaxNumberFind.MaxThreeFloat(29.6f, 141.1f, 34.5f);
+            //Console.WriteLine("Maximum value among them is :" + FloatMaxNumber);
+
+            //// T.C 3st position number is maximim
+            //Console.WriteLine("Using generics find maximum float number among three");
+            //float FloatMaxNumber = MaxNumberFind.MaxThreeFloat(29.6f, 34.5f, 141.1f);
+            //Console.WriteLine("Maximum value among them is :" + FloatMaxNumber);
+
+        }
+    }
+}
